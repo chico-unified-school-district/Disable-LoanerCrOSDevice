359c64ad060886aad79d637bc2dfc9630ce67b75
Justin Cooper <jcooper@chicousd.org>
2019-05-16 11:02:32 -0700

    initial commit

---
+++ Disable-LoanerCrOSDevice.ps1
@@ -0,0 +1,62 @@
+<# 
+.SYNOPSIS
+ Queries Aeries for Chromebooks marked as loaners and disables them at the end of the school day
+.DESCRIPTION
+ Run with Test and/or Log switches as well as common parameters
+.EXAMPLE
+ Update-LoanerCBs.ps1 -SISCOnnection server\database -SISCredential $sisCredObject
+.EXAMPLE
+ Update-LoanerCBs.ps1 -SISCOnnection server\database -SISCredential $sisCredObject -WhatIf -Verbose
+.INPUTS
+ SQL data
+.OUTPUTS
+ Log Messages
+.NOTES
+ Thanks Wendy Kwo for the nice SQL Statements!
+#>
+
+[cmdletbinding()]
+param ( 
+ # SQL server name
+ [Parameter(Mandatory=$True)]
+ [Alias('SISServer')]
+ [string]$SQLServer,
+ # SQL database name
+ [Parameter(Mandatory=$True)]
+ [Alias('SISDatabase','SISDB')]
+ [string]$SQLDatabase,
+ # Aeries SQL user account with SELECT permission to STU table 
+ [Parameter(Mandatory=$True)]
+ [Alias('SISCred')]
+ [System.Management.Automation.PSCredential]$SQLCredential,
+ [switch]$WhatIf
+)
+
+Clear-Host;$error.clear() # Clear Screen and $error.
+
+# Variables
+$gamExe = '.\lib\gam-64\gam.exe'
+
+# Imported Functions
+. .\lib\Add-Log.ps1 # Format Log entries
+. .\lib\Invoke-SqlCommand.ps1 # Useful function for querying SQL and returning results
+
+# Processing
+$crosFields = "deviceId,status,serialNumber"
+
+# Disable
+Write-Host "Check for devices to Disable" -Fore Green
+$disableQuery = Get-Content -Path .\sql\disable.sql -Raw
+$disableLoaners = Invoke-SqlCommand -Server $SQLServer -Database $SQLDatabase -Cred $SQLCredential -Query $disableQuery
+
+foreach ($device in $disableLoaners) {
+  $sn = $device.serialNumber
+  ($crosDev = & $gamExe print cros query "id: $sn" fields $crosFields | ConvertFrom-CSV) *>$null # *>$null suppresses noisy output
+  $id = $crosDev.deviceId
+
+  Write-Debug "Process $sn"
+  if ($crosDev.status -eq "ACTIVE"){ # If cros device set to 'active' then disable
+   Add-Log disable $sn
+   if (!$WhatIf){ & $gamExe update cros $id action disable *>$null }`
+  } else { Write-Verbose "$sn,Skipping. Already Disabled" }
+}
